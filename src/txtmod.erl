12ff4f907313570073ff78c03eba2f6e5d2d2153
Petr Severa <segedacz@gmail.com>
2017-10-06 12:42:27 +0000

    add cs and sk holidays

--- src/txtmod.erl
+++ src/txtmod.erl
@@ -7,7 +7,7 @@
 out(Arg) ->
     case (Arg#arg.req#http_request.method) of
         'GET' ->
-            Svatky = svatkymod:out(1, 2),
+            Svatky = svatkymod:out(),
             Print = fun(Svatek) -> yaws_api:f("~2..0w~2..0w;~s~n", [Svatek#svatek.day, Svatek#svatek.month, binary_to_list(Svatek#svatek.name)]) end,
             {content, "text/plain; charset=utf-8", lists:map(Print, Svatky)};
         _Else ->
