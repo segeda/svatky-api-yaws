fa6aea0c6429a80dcd3c3487b46fd8269104d1fb
Petr Severa <segedacz@gmail.com>
2017-10-09 22:22:00 +0200

    Added filter functions.
    Function `by_name`, `by_month_day`, `by_month` and `by_day`.

--- src/svatkymod.erl
+++ src/svatkymod.erl
@@ -1,20 +1,25 @@
 -module(svatkymod).
 -include("svatek.hrl").
 -include("cs.hrl").
--export([out/0, out/1, out/2]).
+-export([by_name/1, by_month/1, by_day/1, by_month_day/2]).
 
 
-out() ->
-    {_Year, Month, Day} = erlang:date(),
-    out(Month, Day).
-
-
-out(Name) ->
+by_name(Name) ->
     Pred = fun(Svatek) -> Svatek#svatek.name =:= Name end,
     lists:filter(Pred, ?CS).
 
 
-out(Month, Day) ->
+by_month(Month) ->
+    Pred = fun(Svatek) -> Svatek#svatek.month =:= Month end,
+    lists:filter(Pred, ?CS).
+
+
+by_day(Day) ->
+    Pred = fun(Svatek) -> Svatek#svatek.day =:= Day end,
+    lists:filter(Pred, ?CS).
+
+
+by_month_day(Month, Day) ->
     Pred = fun(Svatek) -> Svatek#svatek.month =:= Month andalso Svatek#svatek.day =:= Day end,
     lists:filter(Pred, ?CS).
     
