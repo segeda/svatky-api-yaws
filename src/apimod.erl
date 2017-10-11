c2174f58f56ba304c98f3349eaace57acf7d0494
Petr Severa <segedacz@gmail.com>
2017-10-11 16:33:19 +0200

    Query only params `name`, `month` and `day`

--- src/apimod.erl
+++ src/apimod.erl
@@ -6,27 +6,12 @@
 
 svatky(Arg) ->
     Q = yaws_api:parse_query(Arg),
-    N = case lists:keysearch("name", 1, Q) of
-	  {value, {"name", Name}}
-	      when Name =/= undefined andalso Name =/= [] ->
-	      Name;
-	  _ -> undefined
-	end,
-    M = case lists:keysearch("month", 1, Q) of
-	  {value, {"month", Month}}
-	      when Month =/= undefined andalso Month =/= [] ->
-	      Month;
-	  _ -> undefined
-	end,
-    D = case lists:keysearch("day", 1, Q) of
-	  {value, {"day", Day}}
-	      when Day =/= undefined andalso Day =/= [] ->
-	      Day;
-	  _ -> undefined
-	end,
-    io:format("Name:~p; Month:~p; Day:~p~n", [N, M, D]),
-    {_Year, Month_, Day_} = erlang:date(),
-    svatkymod:by_month_day(Month_, Day_).
+    [io:format("~p:~p~n", [K, V])
+     || {K, V} <- Q,
+	lists:member(K, ["name", "month", "day"]),
+	V =/= undefined andalso V =/= []],
+    {_Year, Month, Day} = erlang:date(),
+    svatkymod:by_month_day(Month, Day).
 
 out(Arg, MimeType, Content) ->
     case (Arg#arg.req)#http_request.method of
