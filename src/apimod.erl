4309ae24531e12970a54eb50944ec961355fb52e
Petr Severa <segedacz@gmail.com>
2017-10-16 11:53:21 +0000

    Added decision tree for query params.
    Priority: name, month and day, month, day.
    Params must be set. Month and day must be integer. Month between 1 and
    12, day between 1 and 31.

--- src/apimod.erl
+++ src/apimod.erl
@@ -6,10 +6,40 @@
 
 svatky(Arg) ->
     Q = yaws_api:parse_query(Arg),
-    [io:format("~p:~p~n", [K, V])
-     || {K, V} <- Q,
-	lists:member(K, ["name", "month", "day"]),
-	V =/= undefined andalso V =/= []],
+    N = case lists:keyfind("name", 1, Q) of
+	  {"name", Name} when Name =/= [] ->
+	      Name_ = list_to_binary(Name), {"name", Name_};
+	  _ -> false
+	end,
+    M = case lists:keyfind("month", 1, Q) of
+	  {"month", Month} when Month =/= [] ->
+	      try list_to_integer(Month) of
+		Month_ when Month_ > 0 andalso Month_ < 13 ->
+		    {"month", Month_};
+		_ -> false
+	      catch
+		_:_ -> false
+	      end;
+	  _ -> false
+	end,
+    D = case lists:keyfind("day", 1, Q) of
+	  {"day", Day} when Day =/= [] ->
+	      try list_to_integer(Day) of
+		Day_ when Day_ > 0 andalso Day_ < 32 -> {"day", Day_};
+		_ -> false
+	      catch
+		_:_ -> false
+	      end;
+	  _ -> false
+	end,
+    by(N, M, D).
+
+by({"name", Name}, _, _) -> svatkymod:by_name(Name);
+by(_, {"month", Month}, {"day", Day}) ->
+    svatkymod:by_month_day(Month, Day);
+by(_, {"month", Month}, _) -> svatkymod:by_month(Month);
+by(_, _, {"day", Day}) -> svatkymod:by_day(Day);
+by(_, _, _) ->
     {_Year, Month, Day} = erlang:date(),
     svatkymod:by_month_day(Month, Day).
 
